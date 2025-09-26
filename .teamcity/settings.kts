797288c014dfac59bb683572dfc784ffe04b27b0
Baryshkov Mikhail <m.baryshkov@mail.ru>
2025-09-26 14:32:19 +0300

    Configure conditional build steps and artifacts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -3,32 +3,9 @@ import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
 version = "2025.07"
 
 project {
-
     buildType(Build)
 }
 
@@ -40,15 +17,27 @@ object Build : BuildType({
     }
 
     steps {
+        // Шаг для master: deploy
         maven {
-            id = "Maven2"
-
+            id = "MavenDeploy"
+            name = "Maven Deploy"
             conditions {
                 contains("teamcity.build.branch", "master")
             }
             goals = "clean deploy"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
             userSettingsSelection = "settings.xml"
+        }
+
+        // Шаг для остальных веток: test
+        maven {
+            id = "MavenTest"
+            name = "Maven Test"
+            conditions {
+                doesNotContain("teamcity.build.branch", "master")
+            }
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
         }
     }
 
@@ -61,4 +50,7 @@ object Build : BuildType({
         perfmon {
         }
     }
+
+    // Пункт 16: артефакты сборки
+    artifactRules = "target/*.jar => artifacts"
 })
