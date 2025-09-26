800d4cb4be9ce4e1dba4f0f545054fef3c464d96
Baryshkov Mikhail <m.baryshkov@mail.ru>
2025-09-26 13:36:49 +0300

    Add test for getHunterReply

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -30,4 +30,10 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
+	@Test
+	public void testHunterReplyContainsWordHunter() {
+  	  Welcomer welcomer = new Welcomer();
+  	  String reply = welcomer.getHunterReply();
+  		  assertTrue(reply.toLowerCase().contains("hunter"));
+	}
 }
